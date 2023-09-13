b6597b111fa80bcb72c9c68a49cc9c15cbf5583d
Ingo Dahn <ingodahn@gmail.com>
2023-09-13 11:50:45 +0200

    Revised copy - gives error

--- assessment/libs/copy.php
+++ assessment/libs/copy.php
@@ -7,58 +7,51 @@ global $allowedmacros;
 array_push($allowedmacros,"copyButton" );
 
 function copyButton($arg="",$label="Copy to clipboard",$successMessage="Copied to clipboard",$failMessage="Copy failed!") {
-    $js_cp="
-    <script>
+  $js_cp="
+  <script>
     function reportSuccess (success) {
       let responseDiv=document.getElementById('js_cp_response');
       let response=(success)?'$successMessage':'$failMessage';
+      console.log(response)
       alert(response);
     }
     function fallbackCopyTextToClipboard(text) {
-        var textArea = document.createElement('textarea');
-        textArea.value = text;
-        
-        // Avoid scrolling to bottom
-        textArea.style.top = '0';
-        textArea.style.left = '0';
-        textArea.style.position = 'fixed';
-      
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
-      
-        try {
-          var successful = document.execCommand('copy');
-          reportSuccess(successful);
-        } catch (err) {
-          console.error('Fallback: Oops, unable to copy', err);
-        }
-      
-        document.body.removeChild(textArea);
+      var textArea = document.createElement('textarea');
+      textArea.value = text;
+      // Avoid scrolling to bottom
+      textArea.style.top = '0';
+      textArea.style.left = '0';
+      textArea.style.position = 'fixed';
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      try {
+        var successful = document.execCommand('copy');
+        reportSuccess(successful);
+      } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
       }
-      function copyTextToClipboard(text) {
+      document.body.removeChild(textArea);
+    }
+    async function copyTextToClipboard(text) {
         if (!navigator.clipboard) {
-          fallbackCopyTextToClipboard(text);
-          return;
-        }
-        navigator.clipboard.writeText(text).then(function() {
-          reportSuccess(true);
-          console.log('Async: Copying to clipboard was successful!');
-        }, function(err) {
-          reportSuccess(false);
-          console.error('Async: Could not copy text: ', err);
-        });
+        fallbackCopyTextToClipboard(text);
+        return;
       }
-      var copyBtn = document.querySelector('.js-copy-btn');
-      copyBtn.addEventListener('click', function(event) {
-        copyTextToClipboard('$arg');
+      try {
+        await navigator.clipboard.writeText(text).then(function() {
+        reportSuccess(true);
+        console.log('Async: Copying to clipboard was successful!');
+      } catch (err) {
+        reportSuccess(false);
+        console.error('Async: Could not copy text: ', err);
       });
-    </script>";
-    return "
-    <div style='display:inline-block; vertical-align:top;'>
-      <button class='js-copy-btn'>$label</button>
-    </div>
-    ".$js_cp;
+    }
+    </script>
+  ";
+  
+  $html = "<button onclick=\"copyTextToClipboard('$arg')\">$label</button>";
+  return $js_cp.$html;
 }
 
 ?>
