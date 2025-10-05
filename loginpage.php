c555c842c6fe3298d7682d63a74e76b3a1cc4e34
sirrio <home@sirrio.de>
2025-10-05 23:43:00 +0200

    Re-added changes to main, so feature_fcrp can stay in sync with its fork

--- loginpage.php
+++ loginpage.php
@@ -6,8 +6,7 @@ if (!isset($imasroot)) { //don't allow direct access to loginpage.php
 //any extra CSS, javascript, etc needed for login page
 	$placeinhead = "<link rel=\"stylesheet\" href=\"$imasroot/infopages.css\" type=\"text/css\" />\n";
 	$placeinhead .= "<script type=\"text/javascript\" src=\"$imasroot/javascript/jstz_min.js\" ></script>";
-	//$nologo = true;
-	//$loadinginfoheader = true; // ID: Kein header über infoheader.php
+	$nologo = true;
 	require("header.php");
 	if (!empty($_SERVER['QUERY_STRING'])) {
 		 $querys = '?'.$_SERVER['QUERY_STRING'];
@@ -22,11 +21,11 @@ if (!isset($imasroot)) { //don't allow direct access to loginpage.php
 		 $challenge = base64_encode(microtime() . rand(0,9999));
 		 $_SESSION['challenge'] = $challenge;
 	 }
-	 $pagetitle = "IMathAS";
+	 $pagetitle = "Home";
 	 include("infoheader.php");
-
+	 
 ?>
-
+	
 
 
 <div id="loginbox">
@@ -35,70 +34,76 @@ if (!isset($imasroot)) { //don't allow direct access to loginpage.php
 	if ($haslogin) {
 		if ($badsession) {
 			if (isset($_COOKIE[session_name()])) {
-				echo _('Problems with session storage');
+				echo 'Problems with session storage';
 			}  else {
-				echo '<p>', _('Unable to establish a session.  Check that your browser is set to allow session cookies'), '</p>';
+				echo '<p>Unable to establish a session.  Check that your browser is set to allow session cookies</p>';
 			}
+		} else if (isset($line['password']) && substr($line['password'],0,8)=='cleared_') {
+			echo '<p>Your password has expired since your account has been unused. Use the Reset Password link below to reset your password.</p>';
 		} else {
-			echo "<p>", _("Login Error.  Try Again"), "</p>\n";
+			echo "<p>Login Error.  Try Again</p>\n";
 		}
 	}
 ?>
-<b>Login</b>
+<b>Login für Dozent:innen</b>
 
 <div><noscript>JavaScript is not enabled.  JavaScript is required for <?php echo $installname; ?>.  Please enable JavaScript and reload this page</noscript></div>
 
 <table>
-<tr><td><label for="username"><?php echo $loginprompt;?></label>:</td><td><input type="text" size="15" id="username" name="username" /></td></tr>
-<tr><td><label for="password"><?php echo _('Password'); ?></label>:</td><td><input type="password" size="15" id="password" name="password" /></td></tr>
+<tr><td><label for="username">Username</label>:</td><td><input type="text" size="15" id="username" name="username" /></td></tr>
+<tr><td><label for="password">Passwort</label>:</td><td><input type="password" size="15" id="password" name="password" /></td></tr>
 </table>
 <div class=textright><input type="submit" value="Login"></div>
 
-<div class="textright"><a href="<?php echo $imasroot; ?>/forms.php?action=newuser"><?php echo 'Als neuer Student registrieren';?></a></br>
-<a href="<?php echo $imasroot; ?>/newinstructor.php"><?php echo 'Als neuer Dozent registrieren';?></a>
-</div>
-<div class="textright"><a href="<?php echo $imasroot; ?>/forms.php?action=resetpw"><?php echo _('Forgot Password');?></a><br/>
-<a href="<?php echo $imasroot; ?>/forms.php?action=lookupusername"><?php echo _('Forgot Username');?></a></div>
+<table>
+<tr><td><a href="mailto:didaktik@hs-rm.de?subject=IMathAS Passwort Vergessen&body=Hallo, Ich habe leider das Passwort für mein IMathAS Konto vergessen. ">Passwort vergessen</a></td></tr>
+<tr><td><a href="mailto:didaktik@hs-rm.de?subject=IMathAS Username Vergessen&body=Hallo, Ich habe leider den Username für mein IMathAS Konto vergessen.">Username vergessen</a></td></tr>
+</table>
+<!-- 
+<div class="textright"><a href="<?php echo $imasroot; ?>/forms.php?action=newuser">Register as a new student</a></div> 
+<div class="textright"><a href="<?php echo $imasroot; ?>/forms.php?action=resetpw">Forgot Password</a><br/>
+<a href="<?php echo $imasroot; ?>/forms.php?action=lookupusername">Forgot Username</a></div>
+-->
 
-<input type="hidden" id="tzoffset" name="tzoffset" value="">
-<input type="hidden" id="tzname" name="tzname" value="">
+<input type="hidden" id="tzoffset" name="tzoffset" value=""> 
+<input type="hidden" id="tzname" name="tzname" value=""> 
 <input type="hidden" id="challenge" name="challenge" value="<?php echo $challenge; ?>" />
-<script type="text/javascript">
+<script type="text/javascript">     
 $(function() {
-        var thedate = new Date();
+        var thedate = new Date();  
         document.getElementById("tzoffset").value = thedate.getTimezoneOffset();
-        var tz = jstz.determine();
+        var tz = jstz.determine(); 
         document.getElementById("tzname").value = tz.name();
         $("#username").focus();
 });
-</script>
+</script>  
 
 </form>
 </div>
 <div class="text">
-	<h1>Die Mathe-Plattform des <a style='color: '#66ff66' href='https://www.vcrp.de' target='_blank'>VCRP</a></h1>
-<p><?php printf('%s ist ein webbasiertes System für Mathematikaufgaben und Kursverwaltung das hier vom <a href="https://www.vcrp.de" target="_blank">Virtuellen Campus Rheinland-Pfalz (VCRP)</a> betrieben wird.', $installname); ?></p>
- <img style="float: left; margin-right: 20px;" src="<?php echo $imasroot; ?>/img/screens.jpg" alt="Computer screens"/>
+<p><b><?php echo $installname; ?> ist ein webbasiertes System für Mathematikaufgaben und Kursverwaltung der Hochschule RheinMain.</b></p>
+ <img style="float: left; margin-right: 20px; padding-top: 10px;" src="<?php echo $imasroot; ?>/img/screens.jpg" alt="Computer screens"/>
 
-<p>Das System wurde für verschiedene Formen von umfangreichen Mathematikaufgaben entworfen. Studenten können durch generierte Tests und automatische Auswertung direkt Rückmeldung bekommen.</p>
+<p>Das System wurde für verschiedene Formen von umfangreichen Mathematikaufgaben entworfen. Studierende können durch generierte Tests und automatische Auswertung direkt Rückmeldung (als numerische oder algebraische Antwort) bekommen.</p>
 
-<p>Wenn Sie bereits ein Benutzerkonto haben, können Sie sich rechts anmelden.</p>
-<p><?php printf('Wenn Sie sich noch nicht registriert haben, können Sie sich <a href="%s/forms.php?action=newuser">als neuer Student registrieren</a>', $imasroot);?>. Für den Kurszugang erhalten Sie von Ihrem Dozenten eine Kurs-ID und ggf. ein Passwort. Viele Kurse sind nur über das Lernmanagementsystem der Hochschulen zugänglich. Für diese Kurse brauchen Sie sich hier nicht zu registrieren.</p>
+<p>Wenn Sie bereits ein Benutzer:innenkonto haben, können Sie sich rechts anmelden.</p>
+<p>Studierende der Hochschule RheinMain gelangen ausschließlich über die hochschuleigenen Plattformen ILIAS oder Stud.IP zu den Kursen in IMathAS.</p>
+<p>Dozent innen der Hochschule RheinMain können unter didaktik(at)hs-rm.de eine Dozent:innenkennung beantragen. Damit erhalten Sie Zugang zur hochschulübergreifenden Aufgabenbibliothek und können eigene Aufgaben und Tests für Ihre Studierenden erstellen.</p>
 
-<p>In öffentliche Kurse können Sie sich auch mit <em>guest</em> ohne Passwort einschreiben. Gast-Kennungen werden regelmäßig gelöscht.</p>
+<!--
+<p>Also available:
+<ul>
+<li><a href="<?php echo $imasroot;?>/info/enteringanswers.php">Help for student with entering answers</a></li>
+<li><a href="<?php echo $imasroot;?>/docs/docs.php">Instructor Documentation</a></li>
+</ul>
+-->
 
-<p><?php printf('Sie können <a href="%s/newinstructor.php"><b>hier eine Dozentenkennung beantragen</b></a>. Für Mitarbeitende und Studierende von Hochschulen des Landes stellt der Virtuelle Campus Rheinland-Pfalz diese Kennung kostenlos zur Verfügung. Mit einer Dozentenkennung erhalten Sie Zugang zur hochschulübergreifenden Aufgabenbibliothek und können eigene Aufgaben und Tests erstellen und in das Lernmanagementsystem Ihrer Hochschule einbinden.', $imasroot)?></p>
-
+<!--
 <br class=clear>
-<p class="textright"><?php printf('%s wird entwickelt von %s', $installname, ' <a href="http://www.imathas.com">IMathAS</a> &copy; 2006-2020 David Lippman');  ?><br>
-<a href="https://netmath.vcrp.de/downloads/Systeme/Impressum.html" target="_blank">Impressum</a> |
+<p class="textright"><?php echo $installname;?> is powered by <a href="http://www.imathas.com">IMathAS</a> &copy; 2006-<?php echo date("Y");?> David Lippman</p>
+-->
+</div> 
 
-<a href="https://netmath.vcrp.de/downloads/Systeme/NutzungsbedingungenIMathAS.html" target="_blank">Nutzungsbedingungen</a> |
-
-<a href="https://netmath.vcrp.de/downloads/Systeme/privacyIMathAS.html" target="_blank">Datenschutzerklärung</a>
-
- </p>
-</div>
-<?php
+<?php 
 	require("footer.php");
 ?>
